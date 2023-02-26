69442d077c77533c4829f6c883485cdf3b4a0fdd
Krit Tangpinyoputtikhun <88623313+tirk101@users.noreply.github.com>
2023-02-27 04:50:08 +0700

    Create item_1.py

---
+++ lab_3_stack/item_1.py
@@ -0,0 +1,28 @@
+class Stack() :
+
+    def __init__(self, list = None) :           
+        if list == None : self.items = []
+        else : self.items = list                
+    
+    def push(self,i) :                                       
+        self.items.append(i)
+    
+    def pop(self) :                           
+        return self.items.pop()                
+    
+    def isEmpty(self) :                        
+        return self.items == []
+    
+    def size(self) :                          
+        return len(self.items)
+
+print(" *** Stack Implement by Python list***")
+ls = [ e for e in input("Enter data stack : ").split()]        
+s = Stack()
+for e in ls :
+    s.push(e)
+print(s.size(),"Data in stack : ",s.items)
+
+while not s.isEmpty() :
+    s.pop()
+print(s.size(),"Data in stack",s.items)
